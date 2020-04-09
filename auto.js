8391a3aaf717b9a8cfb436a9efaa7e3852e54788
kokororin <i@kotori.love>
2020-04-09 17:23:58 +0800

    preact support

--- auto.js
+++ auto.js
@@ -1,7 +1,7 @@
 const { hasAnyDep } = require('ptils');
 
 const typescript = hasAnyDep('typescript') && 'kotori/typescript';
-const react = hasAnyDep('react') && 'kotori/react';
+const react = (hasAnyDep('react') || hasAnyDep('preact')) && 'kotori/react';
 const vue = hasAnyDep('vue') && 'kotori/vue';
 
 module.exports = {
