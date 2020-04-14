b0040910d38715df4d5a1b778c9542bc8aec4b4d
kokororin <i@kotori.love>
2020-04-14 10:29:20 +0800

    Bump

--- typescript.js
+++ typescript.js
@@ -1,9 +1,7 @@
-const path = require('path');
-
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ['tsconfig.json', path.join(__dirname, 'tsconfig-common.json')],
+    project: 'tsconfig.json',
     extraFileExtensions: ['.vue']
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
